4389b2eb769c4212f79abf4f96bd2218579be212
ysys1195 <73346157+ysys1195@users.noreply.github.com>
2022-05-22 22:31:40 +0900

    VR上で撃てるようにする

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // delete object
+        Destroy(gameObject);
+
+        // 爆発のプレハブ
+        Instantiate(explosionPrefab, this.transform.position, Quaternion.identity);
+    }
+}
